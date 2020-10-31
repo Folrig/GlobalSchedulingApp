865ff17d3fa4dbaa7f9877ecdf294349349633ea
James Spencer <james@jamesspencer.io>
2020-10-31 16:35:41 -0700

    Project setup and initial commit

---
+++ GlobalSchedulingApp/src/globalschedulingapp/GlobalSchedulingApp.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package globalschedulingapp;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author James Spencer
+ */
+public class GlobalSchedulingApp extends Application{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/view/LoginView.fxml"));
+        Scene scene = new Scene(root);
+        stage.setTitle("Login");
+        stage.show();
+    }
+}
